7e6b067fa92d0097bb5c03ee409d80948224dee0
Joaopedromenezzes <jpzingames809@gmail.com>
2025-09-01 20:04:29 -0300

    Add files via upload

---
+++ array.py
@@ -0,0 +1,13 @@
+#lista de colegas
+listacolegas = ["João Pedro", "Rosemeire", "dayson", "victor", "jarla"]
+print("primeiro elemento:",listacolegas[0])
+print("segundo elemento:",listacolegas[1]) 
+print("terceiro elemento:",listacolegas[2])
+print("quarto elemento:",listacolegas[3])
+print("quinto elemento:",listacolegas[4])
+
+#desafio extra com IF
+if "carla" in listacolegas:
+    print("carla está na lista!")
+else:
+    print("carla não foi encontrada na lista.")    
